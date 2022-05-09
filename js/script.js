1d9351dba70cd94612ce2b6f5bb0193102c9a8d7
Nick <Nick_II2000@mail.ru>
2022-05-09 21:19:54 +0600

    7-th commit

--- js/script.js
+++ js/script.js
@@ -11,3 +11,9 @@ console.log(btns2[1]);
 
 const circles = document.getElementsByClassName('circle');
 console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+hearts.forEach(item => {
+    console.log(item);
+});
