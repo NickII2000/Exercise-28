182228cdcd4dd9753a5431ebfc2fa058e7a2c2f7
Nick <Nick_II2000@mail.ru>
2022-05-10 18:23:03 +0600

    10-th commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
+// 'use strict';
+
+// const box = document.getElementById('box');
+// console.log(box);
+
+// const btns = document.getElementsByTagName('button')[1];
+// console.log(btns);
+
+// const btns2 = document.getElementsByTagName('button');
+// console.log(btns2[1]);
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+// console.log(hearts);
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+
+// const oneheart = document.querySelector('.heart');
+// console.log(oneheart);
+
+// const div = document.querySelector('div');
+// console.log(div);
+
+// Упражнение 43 (по-новому)
+
 'use strict';
 
-const box = document.getElementById('box');
-console.log(box);
-
-const btns = document.getElementsByTagName('button')[1];
-console.log(btns);
-
-const btns2 = document.getElementsByTagName('button');
-console.log(btns2[1]);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
-hearts.forEach(item => {
-    console.log(item);
-});
-
-const oneheart = document.querySelector('.heart');
-console.log(oneheart);
-
-const div = document.querySelector('div');
-console.log(div);
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll('.heart'),
+    oneheart = document.querySelector('.heart');
+console.dir(box);
