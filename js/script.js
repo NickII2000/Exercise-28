621d5ebb5d32c9b3c838a4c82bfae038169c5723
Nick <Nick_II2000@mail.ru>
2022-05-10 18:44:34 +0600

    16-th commit

--- js/script.js
+++ js/script.js
@@ -43,6 +43,10 @@ box.style.cssText = 'background-color: blue; width: 500px';
 btns[1].style.borderRadius = '100%';
 circles[0].style.backgroundColor = 'red';
 
-for (let i = 0; i <= hearts.length; i++) {
-    hearts[i].style.backgroundColor = 'blue';
-}
+// for (let i = 0; i <= hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'yellow';
+});
